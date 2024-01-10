a14649c997606678c10e37ea6277e94ec5cc2a45
Anna Ma <anna.ma@liferay.com>
2024-01-10 16:11:03 +0800

    Revert "LPS-201769 Recreate indexName each time columns order is susceptible to be changed."
    This reverts commit 8df5175f437f64dfd87cacb23b627395db027dce.

--- portal-kernel/src/com/liferay/portal/kernel/dao/db/IndexMetadata.java
+++ portal-kernel/src/com/liferay/portal/kernel/dao/db/IndexMetadata.java
@@ -151,9 +151,6 @@ public class IndexMetadata extends Index implements Comparable<IndexMetadata> {
 
 				return count2.compareTo(count1);
 			});
-
-		indexName = IndexMetadataFactoryUtil.createIndexName(
-			isUnique(), getTableName(), _columnNames);
 	}
 
 	public Boolean redundantTo(IndexMetadata indexMetadata) {
