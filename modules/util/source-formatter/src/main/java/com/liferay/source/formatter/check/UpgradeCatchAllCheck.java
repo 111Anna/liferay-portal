8cd3f095b8ffdd7e43cf6d8b97bb319085f7d6e8
Anna Ma <anna.ma@liferay.com>
2024-01-10 16:03:56 +0800

    Revert "LPS-204953 Fix on UpgradeCatchAllCheck message case"
    This reverts commit 6ddb3c47dede42534272493c28a539a7ad97b191.

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/UpgradeCatchAllCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/UpgradeCatchAllCheck.java
@@ -53,11 +53,9 @@ public class UpgradeCatchAllCheck extends BaseFileCheck {
 			}
 
 			String from = jsonObject.getString("from");
-			Set<String> keys = jsonObject.keySet();
 
-			if ((from.contains(StringPool.OPEN_PARENTHESIS) &&
-				 !jsonObject.getBoolean("skipParametersValidation")) ||
-				!keys.contains("to")) {
+			if (from.contains(StringPool.OPEN_PARENTHESIS) &&
+				!jsonObject.getBoolean("skipParametersValidation")) {
 
 				expectedMessages.add(_getMessage(jsonObject));
 			}
@@ -167,10 +165,8 @@ public class UpgradeCatchAllCheck extends BaseFileCheck {
 		String[] classNames = JSONUtil.toStringArray(
 			jsonObject.getJSONArray("classNames"));
 
-		String classNamesFormated = null;
-
 		if (classNames.length > 0) {
-			classNamesFormated = StringUtil.merge(classNames, StringPool.SLASH);
+			sb.append(StringUtil.merge(classNames, StringPool.SLASH));
 		}
 
 		String from = jsonObject.getString("from");
@@ -180,8 +176,7 @@ public class UpgradeCatchAllCheck extends BaseFileCheck {
 		if (index != -1) {
 			from = StringUtil.replace(from, CharPool.PERIOD, CharPool.POUND);
 		}
-		else if (classNamesFormated != null) {
-			sb.append(classNamesFormated);
+		else {
 			sb.append(StringPool.POUND);
 		}
 
@@ -230,7 +225,7 @@ public class UpgradeCatchAllCheck extends BaseFileCheck {
 				regex, StringPool.COMMA_AND_SPACE, ",[?\\s\\w]*");
 		}
 		else {
-			regex = regex + "[,;> ({]";
+			regex = regex + "[,;> (]";
 		}
 
 		if (regex.contains(StringPool.PERIOD)) {
@@ -352,16 +347,6 @@ public class UpgradeCatchAllCheck extends BaseFileCheck {
 					to);
 			}
 			else {
-				Set<String> keys = jsonObject.keySet();
-
-				if (!keys.contains("to")) {
-					addMessage(fileName, _getMessage(jsonObject));
-
-					_newMessage = true;
-
-					continue;
-				}
-
 				newContent = StringUtil.replaceFirst(
 					newContent, methodCall,
 					StringUtil.replace(methodCall, from, to));
@@ -443,21 +428,6 @@ public class UpgradeCatchAllCheck extends BaseFileCheck {
 		if (!content.equals(newContent)) {
 			newContent = _addReplacementDependencies(
 				fileName, jsonObject, newContent);
-		}
-		else if (!_newMessage) {
-			Set<String> keys = jsonObject.keySet();
-
-			if (!keys.contains("to")) {
-				Pattern pattern = _getPattern(jsonObject);
-
-				Matcher matcher = pattern.matcher(content);
-
-				if (matcher.find()) {
-					addMessage(fileName, _getMessage(jsonObject));
-
-					_newMessage = true;
-				}
-			}
 		}
 
 		return newContent;
